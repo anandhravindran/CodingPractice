22f37932eb840178dc06efcace121d0cdd039370
Anandh Ravindran <anandh.ravindran@gmail.com>
2015-05-15 02:52:36 +0530

    Created a new activity

--- Sunshine/app/src/main/java/com/example/vlabs/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/example/vlabs/sunshine/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.vlabs.sunshine;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import static com.example.vlabs.sunshine.R.*;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.vlabs.sunshine.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,5 +39,15 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+
     }
 }
